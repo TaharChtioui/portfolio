1cf23939dc757bf83e448589f46c3a3315305a93
TaharChtioui <120992256+TaharChtioui@users.noreply.github.com>
2025-02-04 14:45:57 +0100

    Back to Version Without Netlify Form Integrations

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -56,14 +56,7 @@ function ContactForm() {
   return (
     <div>
       <p className="font-medium mb-5 text-[#16f2b3] text-xl uppercase">Contact me</p>
-      <form
-        className="max-w-3xl text-white rounded-lg border border-[#464c6a] p-3 lg:p-5"
-        data-netlify="true"
-        name="contact"
-        method="POST"
-        onSubmit={handleSendMail}
-      >
-        <input type="hidden" name="form-name" value="contact" />
+      <div className="max-w-3xl text-white rounded-lg border border-[#464c6a] p-3 lg:p-5">
         <p className="text-sm text-[#d3d8e8]">{"If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."}</p>
         <div className="mt-6 flex flex-col gap-4">
           <div className="flex flex-col gap-2">
@@ -116,7 +109,7 @@ function ContactForm() {
             <button
               className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-5 md:px-12 py-2.5 md:py-3 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
               role="button"
-              type="submit"
+              onClick={handleSendMail}
               disabled={isLoading}
             >
               {
@@ -130,7 +123,7 @@ function ContactForm() {
             </button>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
